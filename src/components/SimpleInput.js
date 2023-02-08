12afb5bb6c0252b569fe45de594a70ddc14fe5a7
voyager <cdspacenoob@gmail.com>
2023-02-08 15:53:57 +0900

    control initail validation state

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,8 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  useEffect(() => {
+    if (enteredNameIsValid) {
+      console.log("Name Input is valid");
+    }
+  }, [enteredNameIsValid]);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -11,6 +18,9 @@ const SimpleInput = (props) => {
   const formSubmissionHandler = (event) => {
     // http request가 실행되면 브라우저는 새로고침된다. 그걸 방지해준다.
     event.preventDefault();
+
+    // 공백으로 제출했어도 아무튼 사용자가 의식한 것이다.
+    setEnteredNameTouched(true);
 
     if (enteredName.trim() === "") {
       setEnteredNameIsValid(false);
@@ -24,9 +34,12 @@ const SimpleInput = (props) => {
     setEnteredName("");
   };
 
-  const nameInputClasses = enteredNameIsValid
-    ? "form-control"
-    : "form-control invalid";
+  // 입력된 값이 유효하지 않으며 컴포넌트가 실행된 후 사용자의 조작이 있었을 때, input 값이 유효하지 않은 것으로 판단.
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
 
   return (
     <form onSubmit={formSubmissionHandler}>
@@ -38,7 +51,7 @@ const SimpleInput = (props) => {
           onChange={nameInputChangeHandler}
           value={enteredName}
         />
-        {!enteredNameIsValid && (
+        {nameInputIsInvalid && (
           <p className="error-text">Name must be entered.</p>
         )}
       </div>
